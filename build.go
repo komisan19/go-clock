bbb56f622e9c20c90d7cfa7c6365e08203cf276d
komiyama19 <komiyama6219@gmail.com>
2021-02-17 20:37:38 +0900

    feat: Add go Clock

--- build.go
+++ build.go
@@ -1,22 +1,15 @@
 package main
 
 import (
-	"syscall/js"
+	"fmt"
+	"time"
 )
 
-func clock(this js.Value, args []js.Value) interface{} {
-	date := js.Global().Get("Date").New().Call("toLocaleString").String()
-
-	js.Global().Get("document").Call("getElementById", "clock").Set("textContent", date)
-	return nil
-}
-
-func registerCallbacks() {
-	js.Global().Call("setInterval", js.FuncOf(clock), "200")
+func clock() {
+	now := time.Now()
+	fmt.Println(now)
 }
 
 func main() {
-	c := make(chan struct{}, 0)
-	registerCallbacks()
-	<-c
+	clock()
 }
