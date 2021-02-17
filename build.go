0151f28b57cb8aa032f51fd0d4f07c28a7081cd7
komiyama19 <komiyama6219@gmail.com>
2021-02-17 20:57:02 +0900

    fix: update build

--- build.go
+++ build.go
@@ -7,16 +7,11 @@ import (
 func clock(this js.Value, args []js.Value) interface{} {
 	date := js.Global().Get("Date").New().Call("toLocaleString").String()
 
-	js.Global().Get("document").Call("getElementById", "clock").Set("textContent", date)
+	js.Global().Get("document").Call("getElementById", "clock").Set("innerHTML", date)
 	return nil
 }
 
-func registerCallbacks() {
-	js.Global().Call("setInterval", js.FuncOf(clock), "200")
-}
-
 func main() {
-	c := make(chan struct{}, 0)
-	registerCallbacks()
-	<-c
+	js.Global().Call("setInterval", js.FuncOf(clock), "200")
+	select {}
 }
